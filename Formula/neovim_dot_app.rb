89a8e8bc366bbadc7d6975806f110c1cfbc60fba
Anton Shemerey <shemerey@gmail.com>
2015-04-08 16:15:11 +0300

    initial version

---
+++ Formula/neovim_dot_app.rb
@@ -0,0 +1,26 @@
+class NeovimDotApp < Formula
+  homepage "https://github.com/rogual/neovim-dot-app"
+  head "https://github.com/rogual/neovim-dot-app.git"
+
+  depends_on "neovim" => :build
+  depends_on "scons" => :build
+  depends_on "msgpack" => :build
+  depends_on :python => :recommended if MacOS.version <= :snow_leopard
+
+  def install
+    ENV.deparallelize
+
+    system "make", "install"
+  end
+
+  def caveats; <<-EOS.undent
+      If you want support for Python plugins such as YouCompleteMe, you need
+      to install a Python module in addition to Neovim itself.
+
+      See :h nvim-python-quickstart for more information.
+
+      If you still experiencing some problems with python try to define
+      let g:python_host_prog='/path/to/python' expliciatly in your ~/.nvimrc file
+    EOS
+  end
+end
---
+++ README.md
@@ -0,0 +1,12 @@
+homebrew-neovim-dot-app
+===============
+
+Homebrew formula for Neovim.app (Mac OS X)
+
+## Usage
+
+```bash
+$ brew tap shemerey/homebrew-neovim-dot-app
+$ brew install --HEAD neovim
+```
+
